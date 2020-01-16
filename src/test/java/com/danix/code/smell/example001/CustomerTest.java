f56140197c4a4f47d7804ed2de8a413c02db54c9
unknown <Student@L405-9.carbon.cti.p.lodz.pl>
2020-01-16 09:26:34 +0100

    fixing codesmells

--- src/test/java/com/danix/code/smell/example001/CustomerTest.java
+++ src/test/java/com/danix/code/smell/example001/CustomerTest.java
@@ -1,6 +1,10 @@
 package com.danix.code.smell.example001;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 /**
  * @author  danix
@@ -9,5 +13,8 @@ public class CustomerTest {
 
     @Test
     public void shouldReturnCustomerName() {
+        Customer customer = Mockito.mock(Customer.class);
+        when(customer.getFullName()).thenReturn("Travis");
+        assertEquals("Travis", customer.getFullName());
     }
 }
