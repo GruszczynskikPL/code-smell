54c2b401d84a66acd26416c034bd63f055914b01
unknown <Student@L405-9.carbon.cti.p.lodz.pl>
2020-01-16 09:18:09 +0100

    fixing codesmells

--- src/test/java/com/danix/code/smell/example001/CustomerTest.java
+++ src/test/java/com/danix/code/smell/example001/CustomerTest.java
@@ -1,6 +1,13 @@
 package com.danix.code.smell.example001;
 
+import org.junit.Test;
+
 /**
  * @author  danix
  */
-public class CustomerTest { }
+public class CustomerTest {
+
+    @Test
+    public void shouldReturnCustomerName() {
+    }
+}
